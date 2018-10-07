0c188c5ffe9c044fffb708baa8bc4f316beea927
JanUlrich <andi@michlaustderaffe.de>
2018-10-08 01:22:12 +0200

    First product version

--- app/src/main/java/kuckuck/de/statisticallydrinking/ChoosePlayer.java
+++ app/src/main/java/kuckuck/de/statisticallydrinking/ChoosePlayer.java
@@ -1,7 +1,15 @@
 package kuckuck.de.statisticallydrinking;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class ChoosePlayer extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class ChoosePlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_player);
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        final int PICK_CONTACT = getResources().getInteger(R.integer.PICK_CONTACT_INTENTION);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        final int PICK_CONTACT = getResources().getInteger(R.integer.PICK_CONTACT_INTENTION);
+        if(reqCode == PICK_CONTACT){
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri contactData = data.getData();
+                    Cursor c =  getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        // TODO Whatever you want to do with the selected contact name.
+                    }
+                }
+        }
     }
 }
